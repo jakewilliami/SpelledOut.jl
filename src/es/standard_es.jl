6ad4f40d274dfb1bac7333662e4a3bd82a4893a9
longemen3000 <longemen3000@gmail.com>
2021-01-21 20:58:46 -0300

    add spanish

---
+++ src/es/standard_es.jl
@@ -0,0 +1,95 @@
+const es_1_a_29 =[
+"un"
+"dos"
+"tres"
+"cuatro"
+"cinco"
+"seis"
+"siete"
+"ocho"
+"nueve"
+"diez"
+"once"
+"doce"
+"trece"
+"catorce"
+"quince"
+"dieciséis"
+"diecisiete"
+"dieciocho"
+"diecinueve"
+"veinte"
+"veintiun"
+"veintidós"
+"veintitrés"
+"veinticuatro"
+"veinticinco"
+"veintiséis"
+"veintisiete"
+"veintiocho"
+"veintinueve"
+]
+
+const es_decenas = [
+    ""
+    ""
+    "treinta"
+    "cuarenta"
+    "cincuenta"
+    "sesenta"
+    "setenta"
+    "ochenta"
+    "noventa"
+]
+
+
+const es_centenas = [
+"ciento"
+"doscientos"
+"trescientos"
+"cuatrocientos"
+"quinientos"
+"seiscientos"
+"setecientos"
+"ochocientos"
+"novecientos"
+] 
+
+const es_multiplos_1e6_singular = [
+    "millón"
+    "billón"
+    "trillón"
+    "cuatrillón"
+    "quintillón"
+    "sextillón"
+    "septillón"
+    "octillón"
+    "nonillón"
+    "decillón"
+    "undecillón"
+    "duodecillón"
+]
+
+const es_multiplos_1e6_plural = [
+    "millones"
+    "billones"
+    "trillones"
+    "cuatrillones"
+    "quintillones"
+    "sextillones"
+    "septillones"
+    "octillones"
+    "nonillones"
+    "decillones"
+    "undecillones"
+    "duodecillones"
+]
+
+const es_decimales = [
+    "décimo"
+    "centécimo"
+    "milésimo"
+    "diezmilésimo"
+    "cienmilésimo"
+    "millonésimo"
+]
