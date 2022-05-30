88c1e7517e38b1a2e74da8c373beada70ce938b5
Gabrielforest <gabrielfreitaspereira@estudante.ufscar.br>
2022-05-30 15:56:19 -0300

    added pt_BR

---
+++ src/pt_BR/standard_pt_BR.jl
@@ -0,0 +1,82 @@
+const pt_BR_1_a_19 =[
+    "um"
+    "dois"
+    "três"
+    "quatro"
+    "cinco"
+    "seis"
+    "sete"
+    "oito"
+    "nove"
+    "dez"
+    "onze"
+    "doze"
+    "treze"
+    "catorze"
+    "quinze"
+    "dezesseis"
+    "dezessete"
+    "dezoito"
+    "dezenove"
+]
+
+const pt_BR_dezenas = [
+    ""
+    "vinte"
+    "trinta"
+    "quarenta"
+    "cinquenta"
+    "sessenta"
+    "setenta"
+    "oitenta"
+    "noventa"
+]
+
+const pt_BR_centenas = [
+    "cento"
+    "duzentos"
+    "trezentos"
+    "quatrocentos"
+    "quinhentos"
+    "seiscentos"
+    "setecentos"
+    "oitocentos"
+    "novecentos"
+] 
+
+const pt_BR_multiplos_1e6_singular = [
+    "milhão"
+    "bilhão"
+    "trilhão"
+    "quatrilhão"
+    "quintilhão"
+    "sextilhão"
+    "septilhão" 
+    "oitolhão"
+    "nonilhão" 
+    "decilhão"
+    "undecilhão"
+    "duodecilhão"
+]
+
+const pt_BR_multiplos_1e6_plural = [
+    "milhões"
+    "bilhões"
+    "trilhões"
+    "quatrilhões"
+    "quintilhões"
+    "sextilhões"
+    "septilhões"
+    "oitolhões"
+    "nonilhões"
+    "decilhões"
+    "undecilhões"
+    "duodecilhões"
+]
+
+const pt_BR_decimais = [
+    "décimo"
+    "centésimo"
+    "milésimo"
+    "milionésimo"
+]
