6aa61f913037684b18776d9f45f6e0a5a8739845
Jake Ireland <jakewilliami@icloud.com>
2022-09-22 22:54:10 +1200

    Implement brute force and continue clever solution
    Implement Doov's brute force idea, and continue work on my attempt at
    a somewhat efficient solution (though I think that Doov's idea is
    perhaps the best way, as I can see my solution becoming quite complex)

--- examples/self-referencing-sentence.jl
+++ examples/self-referencing-sentence.jl
@@ -5,17 +5,47 @@ using StatsBase
 
 struct SentenceCountMap
     countmap::Dict{Char, Int}
+    s::String
 end
 
-Base.get(d::SentenceCountMap, k::Char) = get(d.countmap, k, 0)
+Base.getindex(d::SentenceCountMap, k::Char) = get(d.countmap, k, 0)
 Base.setindex!(d::SentenceCountMap, v::Int, k::Char) = setindex!(d.countmap, v, k)
+Base.values(d::SentenceCountMap) = values(d.countmap)
+# Base.iterate(d::SentenceCountMap) = iterate(d.countmap)
+# Base.iterate(d::SentenceCountMap, i::Int) = iterate(d.countmap, i)
 
 SentenceCountMap() = 
-    SentenceCountMap(Dict{Char, Int}(c => 0 for c in 'a':'z'))
+    SentenceCountMap(Dict{Char, Int}(c => 0 for c in 'a':'z'), "")
 SentenceCountMap(s::String) = 
-    SentenceCountMap(countmap(filter(∈('a':'z'), lowercase(s))))
+    SentenceCountMap(countmap(filter(∈('a':'z'), lowercase(s))), s)
 
-function construct_sentence()
+function construct_sentence(cm::SentenceCountMap)
+    io = IOBuffer()
+    print(io, "this sentence has ")
+    for c in 'a':'z'
+        w = spelled_out(cm[c])
+        print(io, "$w '$c's")
+        c == 'z' || print(io, " and ")
+    end
+    return String(take!(io))
+end
+
+function construct_final_sentence_doov()
+    cm = SentenceCountMap()
+    prev = nothing
+    curr = construct_sentence(cm)
+    
+    while true
+        cm = SentenceCountMap(curr)
+        prev = curr
+        curr = construct_sentence(cm)
+        prev == curr && return prev
+    end
+    
+    error("unreachable")
+end
+
+function construct_final_sentence_bonk()
     # Create initial state
     io = IOBuffer()
     print(io, "this sentence has ")
@@ -26,12 +56,29 @@ function construct_sentence()
     s = String(take!(io))
     
     # Construct initial count map and adjust sentence accordingly
-    counts = SentenceCountMap(s)
+    cm = SentenceCountMap(s)
     
-    # TODO: complete example
+    function update!(d::SentenceCountMap, g::Dict{Char, Int})
+        # TODO: this function will only ever _add_ n to the count map,
+        # which is not accurate.  We do still need to account for if, 
+        # for example, 
+        error("not yet implemented")
+        for (c, n) in g
+            d[c] += n
+        end
+        return d
+    end
     
-    return counts
+    # Update counts
+    # TODO: do this until stabilised
+    curr = copy(cm)
+    for n in values(curr)
+        w = spelled_out(n)
+        update!(cm, countmap(w))
+    end
+    
+    return cm
 end
 
-println(construct_sentence())
+# println(construct_sentence())
 
