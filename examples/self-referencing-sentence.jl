4e0630b6f0cb5f269d10e17138af73eb12f03ec9
Jake Ireland <jakewilliami@icloud.com>
2023-01-09 20:10:14 +1300

    Some code clean up within self-referencing example
    In this commit, I dynamically determined when we were at the
    second-to-last or last item in our alphabet, as well as some other
    miscellaneous clean up/improvement.
    
    I had planned to only keep track of one IOBuffer in this commit, but
    after some benchmarking, this did not seem to improve performance:
    
    With one IOBuffer:
        julia> for _ in 1:20
                   @btime construct_true_pangram();
               end
          50.843 s (409455124 allocations: 26.26 GiB)
          765.545 ms (6216088 allocations: 408.26 MiB)
          3.035 s (24595631 allocations: 1.58 GiB)
          33.922 s (273593730 allocations: 17.55 GiB)
          2.409 s (19406731 allocations: 1.24 GiB)
          1.898 s (15389028 allocations: 1010.77 MiB)
          11.832 s (95874131 allocations: 6.15 GiB)
          44.362 s (357949407 allocations: 22.96 GiB)
          2.459 s (19967197 allocations: 1.28 GiB)
          58.991 s (475764808 allocations: 30.52 GiB)
          1.158 s (9389309 allocations: 616.70 MiB)
          25.636 s (207005638 allocations: 13.28 GiB)
          51.847 s (418013672 allocations: 26.81 GiB)
          31.430 s (253300737 allocations: 16.25 GiB)
          40.973 s (327232985 allocations: 20.99 GiB)
          4.422 s (35438747 allocations: 2.27 GiB)
          253.188 ms (2041985 allocations: 134.09 MiB)
          36.172 s (292155055 allocations: 18.74 GiB)
          49.306 s (396551677 allocations: 25.44 GiB)
          55.549 s (446664880 allocations: 28.65 GiB)
    
    With IOBuffer constructed in each construct_pangram call:
        julia> for _ in 1:20
                   @btime construct_true_pangram();
               end
          5.503 s (47207580 allocations: 3.00 GiB)
          7.020 s (60222956 allocations: 3.82 GiB)
          12.165 s (103483280 allocations: 6.57 GiB)
          3.002 s (25651341 allocations: 1.63 GiB)
          5.414 s (46572408 allocations: 2.96 GiB)
          5.389 s (45930841 allocations: 2.92 GiB)
          5.182 s (44124955 allocations: 2.80 GiB)
          9.869 s (85081002 allocations: 5.40 GiB)
          2.006 s (17076024 allocations: 1.08 GiB)
          646.730 ms (5628166 allocations: 365.71 MiB)
          666.316 ms (5935178 allocations: 385.65 MiB)
          1.553 s (13790449 allocations: 896.04 MiB)
          11.421 s (101638768 allocations: 6.45 GiB)
          199.693 ms (1800406 allocations: 117.00 MiB)
          20.385 s (180114986 allocations: 11.43 GiB)
          838.932 ms (7439715 allocations: 483.56 MiB)
          868.218 ms (7668002 allocations: 498.32 MiB)
          9.911 s (87764372 allocations: 5.57 GiB)
          890.219 ms (7936633 allocations: 515.73 MiB)
          11.319 s (100671495 allocations: 6.39 GiB
    
    I will have to investigate this a little further, as I don't think it
    should make such a difference.  Unfortunately, it's a little tricky to
    benchmark with it being random!

--- examples/self-referencing-sentence.jl
+++ examples/self-referencing-sentence.jl
@@ -1,6 +1,9 @@
 # note solution may not converge
 # "This sentence has three 'a's, one 'b, two 'c's, two 'd's, thirty-three 'e's, six 'f's, one 'g, seven 'h's, nine 'i's, one 'j, one 'k, one 'l, one 'm, twenty-two 'n's, fifteen 'o's, one 'p, one 'q, seven 'r's, twenty-seven 's's, sixteen 't's, three 'u's, five 'v's, six 'w's, four 'x's, four 'y's, and one 'z."
 
+
+### Imports
+
 include(joinpath(dirname(@__DIR__), "src", "SpelledOut.jl"))
 using .SpelledOut
 
@@ -8,6 +11,8 @@ using Base: nothing_sentinel, current_logger
 
 using StatsBase: countmap, mean
 
+
+### Constants
 
 const ALPHABET = 'a':'z'
 
@@ -21,6 +26,8 @@ const MIDDLE_STR, MIDDLE_STR_OXFORD = ", ", ", and "
 # const MIDDLE_STR, MIDDLE_STR_OXFORD = ", ", ", and last but not something, exactly"
 const PLURAL_STR = "s"
 
+
+### Helper functions
 
 # there are some letters which never occur in low-valued cardiinals: A,B,C,D,j,K,M,P,Q,Z, so
 # they can be filled in from the initial text directly
@@ -38,18 +45,17 @@ end
 
 
 # Given a count map (i.e., Dict('a' => 3, 'b' => 1, ...)), construct the pangram
-# TODO: Ignore if zero?  NO!  Because then it wouldn't be a pangram
-function construct_pangram(cm, alphabet = ALPHABET, lang = :en)
+function construct_pangram(cm, alphabet = ALPHABET, lang = :en, use_alt_connector::Bool = false)
+    alphabet_len = length(alphabet)
     io = IOBuffer()
     print(io, STARTING_STR)
-    for c in alphabet
+    for (i, c) in enumerate(alphabet)
         n = get(cm, c, 0)
         print(io, spelled_out(n, lang = lang))
         print(io, " '", c)
         n == 1 ? print(io, '\'') : print(io, '\'', PLURAL_STR)
-        c ∈ ('y', 'z') || print(io, MIDDLE_STR)  # TODO: use last and secondtolast here
-        # c == 'y' && print(io, MIDDLE_STR_OXFORD)
-        c == 'y' && print(io, MIDDLE_STR)
+        i >= (alphabet_len - 1) || print(io, MIDDLE_STR)
+        i == (alphabet_len - 1) && print(io, use_alt_connector ? MIDDLE_STR_OXFORD : MIDDLE_STR)
     end
     print(io, ENDING_STR)
 
@@ -69,13 +75,26 @@ function get_static_counts(alphabet = ALPHABET, upper::Int = 40, lang::Symbol = 
     cm′ = Dict{Char, Int}(c => (c ∈ static_chars ? cm[c] : 1) for c in ALPHABET)
     sentence′ = construct_pangram(cm′)
 
-    # println(repr(String(copy(sentence′))))
-    return sentence′
+    println(repr(String(copy(sentence′))))
+    return countmap(sentence′)
 end
 
+
+# There may occur some cycles of count maps that mean this programme never terminates.  As a result, we calculae the difference between
+function rand_cm!(prev_cm, curr_cm)
+    diffs! = mergewith!() do a, b
+        δ = b - a
+        rand(δ < 0 ? (δ:0) : (0:δ)) + a
+    end
+    diffs!(prev_cm, curr_cm)
+    return prev_cm
+end
+
+
+### Main
+
 function construct_true_pangram()
-    # prev_cm = get_static_counts(STARTING_STR, ENDING_STR, MIDDLE_STR, PLURAL_STR)  # TODO: make get_static_counts use construct_pangram
-    prev_cm = get_static_counts()
+    # prev_cm = get_static_counts()
     prev_cm = Dict{Char, Int}()
     i = 0
     while true
@@ -91,17 +110,6 @@ function construct_true_pangram()
 end
 
 
-# There may occur some cycles of count maps that mean this programme never terminates.  As a result, we calculae the difference between
-function rand_cm!(prev_cm, curr_cm, alphabet = ALPHABET)
-    diffs! = mergewith!() do a, b
-        δ = b - a
-        rand(δ < 0 ? (δ:0) : (0:δ)) + a
-    end
-    diffs!(prev_cm, curr_cm)
-    return prev_cm
-end
-
-# Main function: construct true pangram!!
 function main()
     sentence, i = construct_true_pangram()
     println("Found the following sentence in $i iterations:")
@@ -118,4 +126,4 @@ function main()
     println("average = ", mean(S))
 end
 
-# main()
+main()
