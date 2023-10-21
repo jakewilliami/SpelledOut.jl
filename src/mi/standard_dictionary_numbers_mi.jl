db572b3ba8235a1b173ac11a1be006573fa77e33
Jake Ireland <jakewilliami@icloud.com>
2023-10-21 15:57:58 +1300

    WIP: Add notes on Māori

--- src/mi/standard_dictionary_numbers_mi.jl
+++ src/mi/standard_dictionary_numbers_mi.jl
@@ -1,6 +1,13 @@
 const _mi_ten = "tekau"
 const _mi_ma = "mā"
 
+# "kao" = no
+# short for "kaore" = no
+# Can be used to refer to nothing
+# kotahi is used in context, so like "one of x" (whereas tahi is
+# literally the number one)
+# in counting context, kotahi, e rua, e toru, etc. up to e iwa.
+# ordinal is tuatahi, tuarua, etc., up to tuaiwa
 const _mi_small_numbers = String[
     "kore", "tahi", "rua", "toru", "whā", "rima", "ono", "whitu", "waru", "iwa", _mi_ten
 ]
